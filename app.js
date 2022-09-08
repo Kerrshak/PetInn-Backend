54cfaaa807d982dde75560c8dd0676af003ada69
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-09-08 17:08:15 +0100

    remove res.locals

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const session = require("express-session");
 const passport = require("./middleware/passport");
-const bcryptJs = require("bcryptjs");
 const userRouters = require("./routes/user.routes");
 const apiRouter = require("./routes/api.routes");
+const reviewRoutes = require("./routes/review.routes");
 
 dotenv.config();
 app.use(express.json());
@@ -22,13 +22,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use((req, res, next) => {
-  res.locals.currentUser = req.user;
-  next();
-});
-
 // Routes
 app.use("/api", apiRouter);
 app.use("/api/users", userRouters);
+app.use("/api/reviews", reviewRoutes);
 
 module.exports = app;
